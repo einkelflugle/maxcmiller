f97ce7b444a7d73392a00372bb0e94f7aa1b11c0
einkelflugle <cluckyferret@gmail.com>
2014-08-06 19:36:09 +1000

    Added functionality to portfolio section

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,21 @@
-$(function() {
+jQuery(document).ready(function($) {
+
+  delay = 0
+  clicked = false
+
+  $('a[href*=#drawer]').click(function() {
+    nthChild = parseInt($(this).attr('href').substring(7, 8)) + 2
+    if ($('.drawer:nth-child(' + nthChild + ')').css('display') == "none") {
+      if (clicked) {
+        $('.drawer').slideUp(400);
+        delay = 600
+      }
+      $('.drawer:nth-child(' + nthChild + ')').delay(delay).slideToggle(400);
+      clicked = true
+    }
+  });
+
+  // Thanks to CSS-Tricks for the code below
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
