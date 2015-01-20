90799e108d1395c0632b5d0a46d937e282abaf24
einkelflugle <cluckyferret@gmail.com>
2015-01-20 16:31:46 +1000

    Correct indentation of js file

--- javascripts/src/script.js
+++ javascripts/src/script.js
@@ -1,31 +1,32 @@
 jQuery(document).ready(function($) {
 
-  delay = 0
-  clicked = false
+	delay = 0
+	clicked = false
 
-  $('a[href*=#drawer]').click(function() {
-    nthChild = parseInt($(this).attr('href').substring(7, 8)) + 2
-    if ($('.drawer:nth-child(' + nthChild + ')').css('display') == "none") {
-      if (clicked) {
-        $('.drawer').slideUp(400);
-        delay = 600
-      }
-      $('.drawer:nth-child(' + nthChild + ')').delay(delay).slideToggle(400);
-      clicked = true
-    }
-  });
+	$('a[href*=#drawer]').click(function() {
+		nthChild = parseInt($(this).attr('href').substring(7, 8))
+		console.log(" " + nthChild);
+		if ($('.drawer:nth-child(' + nthChild + ')').css('display') == "none") {
+			if (clicked) {
+				$('.drawer').slideUp(400);
+				delay = 600
+			}
+			$('.drawer:nth-child(' + nthChild + ')').delay(delay).slideToggle(400);
+			clicked = true
+		}
+	});
 
-  // Thanks to CSS-Tricks for the code below
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+	// Thanks to CSS-Tricks for the code below
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
 });
